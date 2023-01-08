a3ee4bbeb5993a5952cb40b040517e7a20677c9c
Shubham Shandilya <shubhamshandilya03@gmail.com>
2023-01-08 22:20:23 +0530

    Assignment-4 update

---
+++ Assignment4/script.js
@@ -0,0 +1,10 @@
+var names=["Yakov","John","Jen","jason","paul","frank","larry","paula","laura","jin"];
+
+for(var i=0;i<names.length;i++){
+    if(names[i].charAt(0)==='j'||names[i].charAt(0)==='J'){
+        console.log("Goodbye "+names[i]);
+    }
+    else{
+        console.log("Hello "+names[i]);
+    }
+}
